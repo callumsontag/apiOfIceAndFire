592ac6701f5d0377f4c6770e14dea5dd869e0cba
Callum Sontag <callum.sontag@mettle.co.uk>
2023-05-11 16:24:09 +0100

    added function to query api of ice and fire with custom character ID

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const axios = require("axios");
+
+const characterId = "583";
+
+const characterRequest = (characterId) => {
+  let partUrl = "https://anapioficeandfire.com/api/characters/";
+  let completeUrl = `${partUrl}${characterId}`;
+  axios({
+    method: "get",
+    url: completeUrl,
+  }).then(function (response) {
+    console.log(`Character Name: ${response.data.name}`);
+    console.log(`Played By: ${response.data.playedBy}`);
+  });
+};
+
+characterRequest(characterId);

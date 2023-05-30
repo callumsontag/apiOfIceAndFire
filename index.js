722f23d5046f29a321e7b6dbbe01237d95740746
Callum Sontag <callum.sontag@mettle.co.uk>
2023-05-30 18:02:28 +0100

    refactor html formatting

--- index.js
+++ index.js
@@ -4,29 +4,39 @@ const fs = require("fs");
 const bookId = "https://anapioficeandfire.com/api/books/1";
 
 const main = async () => {
-  const response = await fetchData(bookId);
+  const povCharacters = await fetchPovCharacters(bookId);
 
-  const structuredHtml = await dataToList(response);
+  const listOfChars = await dataToList(povCharacters);
 
-  writeHtml("output.html", structuredHtml);
+  const htmlData = formatHtml(listOfChars);
+
+  writeHtml("output.html", htmlData);
 };
 
-const fetchData = async (apiUrl) => {
+const fetchPovCharacters = async (apiUrl) => {
   const response = await axios(apiUrl);
-  const povCharacters = await response.data.povCharacters;
-  return povCharacters;
+  return response.data.povCharacters;
 };
 
 const dataToList = async (inputData) => {
-  let htmlData = "<div>\n<ul>\n";
+  let charList = "";
   for (let i = 0; i < inputData.length; i++) {
     const character = await axios(inputData[i]);
     const charName = character.data.name;
-    const playedBy = character.data.playedBy;
-    htmlData += `<li>${charName} - ${playedBy}</li>\n`;
+    const charPlayedBy = character.data.playedBy;
+    charList += `${charName} - ${charPlayedBy}\n`;
   }
-  htmlData += "</ul>\n</div>";
-  return htmlData;
+  return charList;
+};
+
+const formatHtml = (input) => {
+  let splitData = input.split(/\r?\n/);
+  let html = "<div>\n<ul>\n";
+  for (let i = 0; i < splitData.length; i++) {
+    html += `<li>${splitData[i]}</li>\n`;
+  }
+  html += "</ul>\n</div>";
+  return html;
 };
 
 const writeHtml = (fileLocation, inputData) => {
